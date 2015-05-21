53ac3cb5140854b8517505539f87507b41057cbf
LarsdeGraaff <lars.dg@gmail.com>
2015-05-21 15:10:31 +0200

    commit

---
+++ src/main/java/be/vdab/Main.java
@@ -0,0 +1,11 @@
+package be.vdab;
+
+/**
+ * Created by jeansmits on 21/05/15.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+        }
+}
